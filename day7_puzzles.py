30ec5e00a98d7893c89a178624a5d4c8009727a4
captainanon <60655724+captainanon@users.noreply.github.com>
2021-12-07 07:21:18 -0700

    add sum of first n integers optimization

--- day7_puzzles.py
+++ day7_puzzles.py
@@ -1,4 +1,7 @@
 import numpy as np
+import time
+
+start = time.time()
 
 file = open('input.txt')
 line = file.readline()
@@ -8,20 +11,21 @@ positions = np.array(positions)
 maximum = max(positions)
 minimum = min(positions)
 
+loss_func = lambda x: x * (x + 1) / 2
 losses = {}
 
 for x in range(minimum, maximum + 1):
-    #loss function for puzzle 1
+    # loss function for puzzle 1
     #loss = np.sum(np.absolute(positions - x))
 
     #loss function for puzzle 2
-    loss = list(np.absolute(positions - x))
-    loss = [sum(range(1, 1 + x)) for x in loss]
-    loss = np.array(loss)
-    loss = np.sum(loss)
+    loss = np.sum(loss_func(np.absolute(positions - x))).astype(np.int64)
     losses[x] = loss
 
 min_loss = min(losses.values())
 min_loss_key = min(losses, key=losses.get)
 
 print(f"x_pos={min_loss_key}, fuel={min_loss}")
+
+end = time.time()
+print(end - start)
